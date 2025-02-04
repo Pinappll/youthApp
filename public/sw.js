d5103cd348a7f93b3ae0537eee5ed9f2857e6b0d
Joshua Grandville <joshua77130@live.fr>
2025-02-04 21:19:55 +0100

    save

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,42 @@
-const CACHE_NAME = 'youth-app-v1';
+const CACHE_NAME = "youth-app-v1";
 const urlsToCache = [
-  '/',
-  './manifest.json',
-  './icons/icon-192x192.png',
-  './icons/icon-512x512.png'
+  "/",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+      .catch((err) => console.error("Cache addAll error:", err))
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      return (
+        response ||
+        fetch(event.request).catch(() => caches.match("/offline.html"))
+      );
+    })
+  );
+});
+
+// Supprimer les anciens caches lors de l'activation du nouveau service worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
     })
   );
 });
