ac9b502e9a01d4b3269267905c9772b6edb8c141
Joshua Grandville <joshua77130@live.fr>
2025-02-11 10:33:24 +0100

    save

--- public/js/pull-to-refresh.js
+++ public/js/pull-to-refresh.js
@@ -3,16 +3,20 @@ let touchEndY = 0;
 const refreshThreshold = 100; // Distance minimale pour déclencher le refresh
 
 document.addEventListener("touchstart", (event) => {
-    touchStartY = event.touches[0].clientY;
+  // Vérifie si l'élément touché est un champ de formulaire
+  if (["INPUT", "TEXTAREA", "SELECT"].includes(event.target.tagName)) {
+    return;
+  }
+  touchStartY = event.touches[0].clientY;
 });
 
 document.addEventListener("touchmove", (event) => {
-    touchEndY = event.touches[0].clientY;
+  touchEndY = event.touches[0].clientY;
 });
 
 document.addEventListener("touchend", () => {
-    // Vérifie si l'utilisateur est en haut de la page et a glissé vers le bas
-    if (window.scrollY === 0 && touchEndY - touchStartY > refreshThreshold) {
-        location.reload(); // Recharge la page
-    }
+  // Vérifie si l'utilisateur est en haut de la page et a glissé vers le bas
+  if (window.scrollY === 0 && touchEndY - touchStartY > refreshThreshold) {
+    location.reload(); // Recharge la page
+  }
 });
