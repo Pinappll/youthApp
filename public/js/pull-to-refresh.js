1111e357a5ff8bb823ad73c207e355bf8fc02ee2
Joshua Grandville <joshua77130@live.fr>
2025-02-12 10:04:45 +0100

    save

--- public/js/pull-to-refresh.js
+++ public/js/pull-to-refresh.js
@@ -1,37 +1,37 @@
-let touchStartY = 0;
-let touchEndY = 0;
-let threshold = 100; // Distance minimale pour détecter un pull-down
+// let touchStartY = 0;
+// let touchEndY = 0;
+// let threshold = 100; // Distance minimale pour détecter un pull-down
 
-window.addEventListener(
-  "touchstart",
-  function (event) {
-    if (
-      event.target.tagName.toLowerCase() === "input" ||
-      event.target.tagName.toLowerCase() === "textarea"
-    ) {
-      return;
-    }
-    touchStartY = event.touches[0].clientY;
-  },
-  { passive: true } // Ajoute ceci pour éviter de bloquer le scroll natif
-);
+// window.addEventListener(
+//   "touchstart",
+//   function (event) {
+//     if (
+//       event.target.tagName.toLowerCase() === "input" ||
+//       event.target.tagName.toLowerCase() === "textarea"
+//     ) {
+//       return;
+//     }
+//     touchStartY = event.touches[0].clientY;
+//   },
+//   { passive: true } // Ajoute ceci pour éviter de bloquer le scroll natif
+// );
 
-window.addEventListener("touchend", function (event) {
-  if (
-    event.target.tagName.toLowerCase() === "input" ||
-    event.target.tagName.toLowerCase() === "textarea"
-  ) {
-    return;
-  }
+// window.addEventListener("touchend", function (event) {
+//   if (
+//     event.target.tagName.toLowerCase() === "input" ||
+//     event.target.tagName.toLowerCase() === "textarea"
+//   ) {
+//     return;
+//   }
 
-  touchEndY = event.changedTouches[0].clientY;
+//   touchEndY = event.changedTouches[0].clientY;
 
-  if (
-    touchEndY - touchStartY > threshold &&
-    document.visibilityState === "visible" &&
-    window.scrollY === 0
-  ) {
-    console.log("Pull-to-refresh detected at top of page");
-    window.location.reload();
-  }
-});
+//   if (
+//     touchEndY - touchStartY > threshold &&
+//     document.visibilityState === "visible" &&
+//     window.scrollY === 0
+//   ) {
+//     console.log("Pull-to-refresh detected at top of page");
+//     window.location.reload();
+//   }
+// });
